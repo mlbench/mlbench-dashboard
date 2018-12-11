d1f03107505266354a7690b896e9ad200571418f
Ralf Grubenmann <ralf.grubenmann@gmail.com>
2018-12-11 12:23:22 +0100

    Fixes worker slider ticks

--- src/main/views.py
+++ src/main/views.py
@@ -35,9 +35,9 @@ def runs(request):
         max_cpu = int(max_cpu)
 
     max_workers = int(max_workers)
-    worker_ticks = [str(i ** 2) for i in range(
-        1,
-        math.floor(math.sqrt(max_workers)))]
+    worker_ticks = [str(2 ** i) for i in range(
+        0,
+        math.floor(math.log2(max_workers)) + 1)]
     # In runs.html we have a hardcoded bound 10000.
     max_bandwidth = max(int(float(max_bandwidth)), 10000)
 
