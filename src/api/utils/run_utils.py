7f2167fc95e86e41d80b4929acaddfd29da9d7c3
Ralf Grubenmann <ralf.grubenmann@gmail.com>
2019-11-25 19:01:14 +0100

    Run multiple experiments (almost working)

--- src/api/utils/run_utils.py
+++ src/api/utils/run_utils.py
@@ -4,65 +4,231 @@ import kubernetes.stream as stream
 from rq import get_current_job
 
 import os
+from copy import deepcopy
 from time import sleep
+import traceback
+
+MAX_POD_RETRIES = 20
+
+service_template = client.V1Service(
+    api_version="v1",
+    kind="Service",
+    metadata=client.V1ObjectMeta(
+        name="",
+        labels={
+            "app": "mlbench",
+            "chart": "mlbench-2.0.0",
+            "component": "worker",
+            "release": os.environ.get('MLBENCH_KUBE_RELEASENAME'),
+            "heritage": "Helm",
+            "set": ""}),
+    spec=client.V1ServiceSpec(
+        selector={
+            "app": "mlbench",
+            "release": os.environ.get('MLBENCH_KUBE_RELEASENAME'),
+            "set": ""
+        },
+        cluster_ip="None",
+        ports=[
+            client.V1ServicePort(
+                name="dummy",
+                port=1234,
+                target_port=1234)]))
+
+
+statefulset_template = client.V1beta2StatefulSet(
+    api_version="apps/v1beta2",
+    kind="StatefulSet",
+    metadata=client.V1ObjectMeta(
+        name="",
+        labels={
+            "app": "mlbench",
+            "chart": "mlbench-2.0.0",
+            "component": "worker",
+            "release": os.environ.get('MLBENCH_KUBE_RELEASENAME'),
+            "heritage": "Helm",
+            "set": ""}),
+    spec=client.V1beta2StatefulSetSpec(
+        replicas=0,
+        selector=client.V1LabelSelector(
+            match_labels={
+                "app": "mlbench",
+                "release": os.environ.get('MLBENCH_KUBE_RELEASENAME'),
+                "set": ""
+            }
+        ),
+        service_name="",
+        pod_management_policy="Parallel",
+        update_strategy=client.V1beta2StatefulSetUpdateStrategy(
+            type="RollingUpdate"),
+        template=client.V1PodTemplateSpec(
+            metadata=client.V1ObjectMeta(
+                labels={
+                    "app": "mlbench",
+                    "chart": "mlbench-2.0.0",
+                    "component": "worker",
+                    "release": os.environ.get('MLBENCH_KUBE_RELEASENAME'),
+                    "heritage": "Helm",
+                    "set": ""}),
+            spec=client.V1PodSpec(
+                service_account_name="",
+                affinity=client.V1Affinity(
+                    pod_anti_affinity=client.V1PodAntiAffinity(
+                        required_during_scheduling_ignored_during_execution=[
+                            client.V1PodAffinityTerm(
+                                label_selector=client.V1LabelSelector(
+                                    match_expressions=[
+                                        client.V1LabelSelectorRequirement(
+                                            key="component",
+                                            operator="In",
+                                            values=["worker"]
+                                        )
+                                    ]
+                                ),
+                                topology_key="kubernetes.io/hostname"
+                            )
+                        ]
+                    )
+                ),
+                containers=[
+                    client.V1Container(
+                        name="",
+                        image="",
+                        image_pull_policy="Always",
+                        stdin=True,
+                        tty=True,
+                        ports=[
+                            client.V1ContainerPort(
+                                name="ssh",
+                                container_port=22,
+                                host_port=16166,
+                                protocol="TCP"
+                            )
+                        ],
+                        resources=client.V1ResourceRequirements(
+                            limits={
+                                "cpu": "1",
+                                "nvidia.com/gpu": "0"
+                            }
+                        ),
+                        volume_mounts=[
+                            client.V1VolumeMount(
+                                name="mlbench-ssh-key",
+                                mount_path="/ssh-key/root"
+                            )
+                        ],
+                        security_context=client.V1SecurityContext(
+                            privileged=True
+                        )
+                    )
+                ],
+                volumes=[
+                    client.V1Volume(
+                        name="mlbench-ssh-key",
+                        secret=client.V1SecretVolumeSource(
+                            secret_name="{}-ssh-key".format(
+                                os.environ.get('MLBENCH_KUBE_RELEASENAME')),
+                            default_mode=256
+                        )
+                    )
+                ]
+            )
+        )
+    )
+)
 
 
 def create_statefulset(model_run, name, namespace, job):
-    kube_api = client.AppsV1beta1Api()
+    core = client.CoreV1Api()
+    kube_api = client.AppsV1beta2Api()
 
-    statefulset_name = "{0}-mlbench-worker-{1}".format(name, model_run.name)
+    statefulset_name = "{1}-mlbench-worker-{0}".format(name, model_run.name)
 
-    template_name = "{}-mlbench-worker".format(name)
+    # create service
+    service = deepcopy(service_template)
 
-    statefulset = kube_api.read_namespaced_stateful_set(
-        template_name, namespace)
+    service.metadata.name = statefulset_name
+    service.metadata.labels['set'] = model_run.name
+    service.spec.selector['set'] = model_run.name
+
+    response = core.create_namespaced_service(namespace, service)
+
+    # create stateful set
+    statefulset = deepcopy(statefulset_template)
 
     statefulset.metadata.name = statefulset_name
+    statefulset.metadata.labels['set'] = model_run.name
+
+    statefulset.spec.selector.match_labels['set'] = model_run.name
+    statefulset.spec.service_name = "mlbench-worker-{}".format(model_run.name)
     statefulset.spec.replicas = int(model_run.num_workers)
-    statefulset.spec.template.spec.containers[0].resources.limits.cpu = \
+    statefulset.spec.template.spec.containers[0].resources.limits['cpu'] =\
         model_run.cpu_limit
     statefulset.spec.template.spec.containers[0].image = model_run.image
+    statefulset.spec.template.spec.containers[0].name = "{}-worker".format(
+        model_run.name)
+    statefulset.spec.template.metadata.labels['set'] = model_run.name
 
-    kube_api.create_namespaced_stateful_set(namespace, statefulset)
+    response = kube_api.create_namespaced_stateful_set(namespace,
+                                                       statefulset)
+
+    job.meta['stdout'].append("Waiting for pods to become available\n")
+    job.save()
 
     # wait for StatefulSet to be created
     while True:
         response = kube_api.read_namespaced_stateful_set_status(
             statefulset_name, namespace)
         s = response.status
-        if (s.current_replicas == response.spec.replicas and
-                s.replicas == response.spec.replicas and
-                s.observed_generation >= response.metadata.generation):
-            break
 
         job.meta['stdout'].append(
             "Waiting for workers: Current: {}/{}, Replicas: {}/{}, "
+            "Ready: {}, "
             "Observed Gen: {}/{}".format(
                 s.current_replicas,
                 response.spec.replicas,
                 s.replicas,
                 response.spec.replicas,
+                s.ready_replicas,
                 s.observed_generation,
                 response.metadata.generation
             ))
         job.save()
 
+        if (s.current_replicas == response.spec.replicas and
+                s.replicas == response.spec.replicas and
+                s.ready_replicas == response.spec.replicas and
+                s.observed_generation == response.metadata.generation):
+            break
+
         sleep(1)
 
+    return statefulset_name
 
-def delete_statefulset(model_run, name, namespace):
+
+def delete_statefulset(statefulset_name, namespace):
     kube_api = client.AppsV1beta1Api()
 
-    statefulset_name = "{0}-mlbench-worker-{1}".format(name, model_run.name)
+    kube_api.delete_namespaced_stateful_set(statefulset_name, namespace,
+                                            body=client.V1DeleteOptions())
 
-    kube_api.delete_namespaced_stateful_set(statefulset_name, namespace)
+
+def delete_service(statefulset_name, namespace):
+    kube_api = client.CoreV1Api()
+
+    kube_api.delete_namespaced_service(statefulset_name, namespace,
+                                       body=client.V1DeleteOptions())
 
 
-def check_nodes_available_for_execution(model_run):
+def check_nodes_available_for_execution(model_run, job):
     from api.models import ModelRun
 
-    max_workers = os.environ.get('MLBENCH_MAX_WORKERS')
+    job.meta['stdout'].append("Waiting for nodes to be available\n")
+    job.save()
+
+    max_workers = int(os.environ.get('MLBENCH_MAX_WORKERS'))
     active_runs = ModelRun.objects.filter(state=ModelRun.STARTED)
+    print(list(active_runs))
 
     utilized_workers = sum(r.num_workers for r in active_runs)
 
@@ -73,7 +239,7 @@ def check_nodes_available_for_execution(model_run):
 
     pending_runs = ModelRun.objects.filter(
         state=ModelRun.INITIALIZED).order_by('num_workers')
-
+    print(list(pending_runs))
     for r in pending_runs:
         if r.num_workers > available_workers:
             return False
@@ -97,15 +263,25 @@ def run_model_job(model_run):
 
     from api.models import ModelRun, KubePod
 
+    release_name = os.environ.get('MLBENCH_KUBE_RELEASENAME')
+    ns = os.environ.get('MLBENCH_NAMESPACE')
+
+    job = get_current_job()
+
+    job.meta['stdout'] = []
+    job.meta['stderr'] = []
+    job.meta['stdout'].append("Initializing run")
+    job.save()
+
+    model_run.job_id = job.id
+    model_run.save()
+
+    set_name = ''
+
     try:
-        while not check_nodes_available_for_execution(model_run):
+        while not check_nodes_available_for_execution(model_run, job):
             sleep(30)
-        job = get_current_job()
 
-        job.meta['stdout'] = []
-        job.meta['stderr'] = []
-
-        model_run.job_id = job.id
         model_run.state = ModelRun.STARTED
         model_run.save()
 
@@ -113,29 +289,49 @@ def run_model_job(model_run):
 
         v1 = client.CoreV1Api()
 
-        release_name = os.environ.get('MLBENCH_KUBE_RELEASENAME')
-        ns = os.environ.get('MLBENCH_NAMESPACE')
+        set_name = create_statefulset(model_run, release_name, ns, job)
 
-        create_statefulset(model_run, release_name, ns, job)
+        job.meta['stdout'].append("Created stateful set, starting run.")
+        job.save()
 
         # start run
         ret = v1.list_namespaced_pod(
             ns,
-            label_selector="component=worker,app=mlbench,release={}"
-            .format(release_name))
+            label_selector="component=worker,app=mlbench,release={0},set={1}"
+            .format(release_name, model_run.name))
 
-        pods = []
-        db_pods = []
-        hosts = []
-        for i in ret.items:
-            pods.append((i.status.pod_ip,
-                         i.metadata.namespace,
-                         i.metadata.name,
-                         str(i.metadata.labels)))
+        retries = 0
+        while retries < MAX_POD_RETRIES:
+            if len(ret.items) == 0:
+                sleep(10)
+                ret = v1.list_namespaced_pod(
+                    ns,
+                    label_selector=\
+                        "component=worker,app=mlbench,release={0},set={1}"
+                    .format(release_name, model_run.name))
+                continue
+            pods = []
+            db_pods = []
+            hosts = []
+            for i in ret.items:
+                pods.append((i.status.pod_ip,
+                            i.metadata.namespace,
+                            i.metadata.name,
+                            str(i.metadata.labels)))
+                try:
+                    db_pod = KubePod.objects.get(name=i.metadata.name)
+                    db_pods.append(db_pod)
+                    hosts.append("{}.{}".format(i.metadata.name, release_name))
+                except KubePod.DoesNotExist:
+                    sleep(10)
+                    retries += 1
+                    break  # wait for pods to be in DB
 
-            db_pod = KubePod.objects.get(name=i.metadata.name)
-            db_pods.append(db_pod)
-            hosts.append("{}.{}".format(i.metadata.name, release_name))
+            if len(hosts) > 0:
+                break
+
+        if retries == MAX_POD_RETRIES:
+            raise Exception("Couldn't find pods in db")
 
         model_run.pods.set(db_pods)
         model_run.save()
@@ -194,6 +390,9 @@ def run_model_job(model_run):
             if not model_run.run_on_all_nodes:
                 break
 
+        job.meta['stdout'].append("Started run.")
+        job.save()
+
         # keep writing openmpi output to job metadata
         while any(s.is_open() for s in streams):
             for s in streams:
@@ -211,11 +410,13 @@ def run_model_job(model_run):
 
         model_run.state = ModelRun.FINISHED
         model_run.save()
-    except Exception as e:
+    except (Exception, BaseException):
         model_run.state = ModelRun.FAILED
-
-        job.meta['stderr'].append(str(e))
+        job.meta['stderr'].append("Run failed")
+        job.meta['stderr'].append(traceback.format_exc())
         job.save()
         model_run.save()
     finally:
-        delete_statefulset(model_run, release_name, ns)
+        if set_name:
+            delete_statefulset(set_name, ns)
+            delete_service(set_name, ns)
